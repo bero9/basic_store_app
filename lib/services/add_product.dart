10b27b1e1affac458fe48d31e52af054bb0b6472
bero9 <berohamood545@gmail.com>
2024-03-23 14:46:03 +0300

    ADD addProduct method in AddProduct class to create new products with ProductModels mapping

---
+++ lib/services/add_product.dart
@@ -0,0 +1,23 @@
+import 'package:storeapp/helpers/api_function.dart';
+
+import '../models/product_models.dart';
+
+class AddProduct {
+  Future<ProductModels> addProduct({ required String title,
+    required String price,
+    required String description,
+    required String image,
+    required String category}) async {
+  Map<String,dynamic> data  =await Api().post(
+        url: 'https://fakestoreapi.com/products', body: {
+      'title': title,
+      'price': price,
+      'description': description,
+      'image': image,
+      'category': category,
+    }, ); 
+ return ProductModels.fromJson(data);
+  }
+}
+
+
