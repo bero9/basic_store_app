ffdd068784a33311153554f058eacc6a0ea8a9c0
bero9 <berohamood545@gmail.com>
2024-03-23 15:04:56 +0300

    ADD token authorization to get method and import foundation.dart in Api class

--- lib/helpers/api_function.dart
+++ lib/helpers/api_function.dart
@@ -1,11 +1,16 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
-  Future<dynamic> get({required String url}) async {
-    http.Response response = await http.get(Uri.parse(url));
+  Future<dynamic> get({required String url, @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    http.Response response = await http.get(Uri.parse(url), headers: headers);
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
     } else {
@@ -36,17 +41,17 @@ class Api {
 
   Future<dynamic> put(
       {required String url,
-        @required dynamic body,
-        @required String? token}) async {
+      @required dynamic body,
+      @required String? token}) async {
     Map<String, String> headers = {};
     headers.addAll({
-'Content-Type' : 'application/x-www-form-urlencoded',
+      'Content-Type': 'application/x-www-form-urlencoded',
     });
     if (token != null) {
       headers.addAll({'Authorization': 'Bearer $token'});
     }
     http.Response response =
-    await http.post(Uri.parse(url), body: body, headers: headers);
+        await http.post(Uri.parse(url), body: body, headers: headers);
     if (response.statusCode == 200) {
       Map<String, dynamic> data = jsonDecode(response.body);
 
