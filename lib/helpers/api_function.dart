39a595f965ddfbbba113c5c7f4711014a74cbb9e
bero9 <berohamood545@gmail.com>
2024-03-23 14:45:24 +0300

    ADD status code check in post method to handle non-200 responses with error details in Api class

--- lib/helpers/api_function.dart
+++ lib/helpers/api_function.dart
@@ -20,14 +20,17 @@ class Api {
       @required String? token}) async {
     Map<String, String> headers = {};
     if (token != null) {
-      headers.addAll({
-        'Authorization' : 'Bearer $token'
-      });
+      headers.addAll({'Authorization': 'Bearer $token'});
     }
     http.Response response =
         await http.post(Uri.parse(url), body: body, headers: headers);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
 
-    Map<String, dynamic> data = jsonDecode(response.body);
-    return data;
+      return data;
+    } else {
+      throw Exception(
+          'there is a problem with status code ${response.statusCode} with body ${jsonDecode(response.body)}');
+    }
   }
 }
